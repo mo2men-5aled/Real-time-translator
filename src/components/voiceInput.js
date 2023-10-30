b8a6f39873e0477e3d73141c0cf6f19969718595
Momen <momenk208@gmail.com>
2023-10-30 04:54:24 +0200

    added some checks to check if the browser supports the mic

--- src/components/voiceInput.js
+++ src/components/voiceInput.js
@@ -30,57 +30,75 @@ const AudioStreamComponent = ({
 
   const startAudioStream = async () => {
     try {
-      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-      const recorder = new MediaRecorder(stream);
-      const audioChunks = [];
+      const permissionStatus = await navigator.permissions.query({
+        name: 'microphone',
+      });
 
-      recorder.ondataavailable = e => {
-        if (e.data.size > 0) {
-          audioChunks.push(e.data);
-        }
+      if (permissionStatus.state === 'denied') {
+        alert('Microphone permission denied');
+        return;
+      }
 
-        const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
+      if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+        });
 
-        // Convert audioBlob to Base64
-        const reader = new FileReader();
-        reader.readAsDataURL(audioBlob);
-        reader.onload = () => {
-          const audioBase64 = reader.result;
+        const recorder = new MediaRecorder(stream);
+        const audioChunks = [];
 
-          // prepare the data to send to the server
-          const data = {
-            from: fromLanguage,
-            to: toLanguage,
-            data: audioBase64,
-          };
-
-          if (websocket && websocket.readyState === WebSocket.OPEN) {
-            websocket.send(JSON.stringify(data));
-            websocket.onmessage = e => {
-              const data = JSON.parse(e.data);
-              setText(data.text);
-              setTranslation(data.translation);
-              setTranslationHighlightWords(data.translationHighlightWords);
-              setSpeechHighlightWords(data.speechHighlitedWords);
-            };
+        recorder.ondataavailable = e => {
+          if (e.data.size > 0) {
+            audioChunks.push(e.data);
           }
+
+          const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
+
+          // Convert audioBlob to Base64
+          const reader = new FileReader();
+          reader.readAsDataURL(audioBlob);
+          reader.onload = () => {
+            const audioBase64 = reader.result;
+
+            // prepare the data to send to the server
+            const data = {
+              from: fromLanguage,
+              to: toLanguage,
+              data: audioBase64,
+            };
+
+            if (websocket && websocket.readyState === WebSocket.OPEN) {
+              websocket.send(JSON.stringify(data));
+              websocket.onmessage = e => {
+                const data = JSON.parse(e.data);
+                setText(data.text);
+                setTranslation(data.translation);
+                setTranslationHighlightWords(data.translationHighlightWords);
+                setSpeechHighlightWords(data.speechHighlitedWords);
+              };
+            }
+          };
         };
-      };
 
-      recorder.onstop = () => {
-        stream.getTracks().forEach(track => track.stop());
-        setAudioStream(null);
-        setMediaRecorder(null);
-      };
+        recorder.onstop = () => {
+          stream.getTracks().forEach(track => track.stop());
+          setAudioStream(null);
+          setMediaRecorder(null);
+        };
 
-      recorder.start();
+        recorder.start();
 
-      setAudioStream(stream);
-      setMediaRecorder(recorder);
-      setIsSpeaking(true);
+        setAudioStream(stream);
+        setMediaRecorder(recorder);
+        setIsSpeaking(true);
+      } else {
+        console.error('Browser does not support getUserMedia');
+      }
     } catch (error) {
       console.error('Error accessing the microphone:', error);
     }
+
+    // check if mediaDevices is available for the browser
   };
 
   const stopAudioStream = () => {
