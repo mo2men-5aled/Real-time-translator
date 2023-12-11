ba9fa702d312c6ba143955bfac6606983341c4ee
Momen <momenk208@gmail.com>
2023-12-11 03:55:30 +0200

    update the media view box and video player to start recording om play the video and stop it onpause the video or audio

--- src/components/MediaViewBox.js
+++ src/components/MediaViewBox.js
@@ -1,62 +1,109 @@
-import { useRef } from 'react';
+import React, { useRef } from 'react';
 
 import { HStack, Box, Text } from '@chakra-ui/react';
 
 import VideoPlayer from './VideoPlayer';
 
-const MediaViewBox = ({ selectedFile }) => {
-  const mediaRef = useRef(null);
-  return (
-    <Box width={'full'} mb={'1rem'}>
-      {selectedFile && (
-        <Box width={'full'} borderWidth="0.1rem" borderRadius="md" padding={8}>
-          <HStack
+const MediaViewBox = React.memo(
+  ({
+    selectedFile,
+    fromLanguage,
+    setText,
+    useSpeechRecognition,
+    SpeechRecognition,
+    setIsRecording,
+    listening,
+    transcript,
+  }) => {
+    const mediaRef = useRef(null);
+
+    const handlePlay = () => {
+      // Start speech recognition when the video begins playing
+      if (!listening) {
+        SpeechRecognition.startListening({
+          continuous: true,
+          language: fromLanguage, // Set the appropriate language
+        });
+        setIsRecording(true);
+      }
+    };
+
+    const handlePause = () => {
+      // Stop speech recognition when the video is paused
+      if (listening) {
+        SpeechRecognition.stopListening();
+        setIsRecording(false);
+      }
+    };
+
+    // setText on transcription change
+    React.useEffect(() => {
+      setText(transcript);
+    }, [transcript, setText]);
+
+    return (
+      <Box width={'full'} mb={'1rem'}>
+        {selectedFile && (
+          <Box
             width={'full'}
-            justifyContent={'center'}
-            alignItems={'center'}
-            marginBottom={'1rem'}
+            borderWidth="0.1rem"
+            borderRadius="md"
             padding={8}
           >
-            <Text
-              fontWeight="bold"
-              fontSize={'lg'}
-              textAlign={'center'}
+            <HStack
               width={'full'}
+              justifyContent={'center'}
+              alignItems={'center'}
+              marginBottom={'1rem'}
+              padding={8}
             >
-              Selected File:
-            </Text>
-            <Text
-              width={'full'}
-              fontWeight="bold"
-              fontSize={'lg'}
-              textAlign={'center'}
-              color={'blue.400'}
-            >
-              {' '}
-              {selectedFile.name}
-            </Text>
-          </HStack>
+              <Text
+                fontWeight="bold"
+                fontSize={'lg'}
+                textAlign={'center'}
+                width={'full'}
+              >
+                Selected File:
+              </Text>
+              <Text
+                width={'full'}
+                fontWeight="bold"
+                fontSize={'lg'}
+                textAlign={'center'}
+                color={'blue.400'}
+              >
+                {' '}
+                {selectedFile.name}
+              </Text>
+            </HStack>
 
-          {selectedFile.type.startsWith('audio/') ? (
-            <audio
-              ref={mediaRef}
-              controls
-              style={{
-                width: '100%',
-              }}
-            >
-              <source
-                src={URL.createObjectURL(selectedFile)}
-                type={selectedFile.type}
+            {selectedFile.type.startsWith('audio/') ? (
+              <audio
+                ref={mediaRef}
+                controls
+                style={{
+                  width: '100%',
+                }}
+                onPlay={handlePlay}
+                onPause={handlePause}
+              >
+                <source
+                  src={URL.createObjectURL(selectedFile)}
+                  type={selectedFile.type}
+                />
+              </audio>
+            ) : (
+              <VideoPlayer
+                url={URL.createObjectURL(selectedFile)}
+                handlePause={handlePause}
+                handlePlay={handlePlay}
               />
-            </audio>
-          ) : (
-            <VideoPlayer url={URL.createObjectURL(selectedFile)} />
-          )}
-        </Box>
-      )}
-    </Box>
-  );
-};
+            )}
+          </Box>
+        )}
+      </Box>
+    );
+  }
+);
 
 export default MediaViewBox;
--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -2,7 +2,7 @@ import { Box } from '@chakra-ui/react';
 import React from 'react';
 import ReactPlayer from 'react-player';
 
-const VideoPlayer = ({ url }) => {
+const VideoPlayer = ({ url, handlePause, handlePlay }) => {
   return (
     <Box width={'full'} position={'relative'} borderRadius={'lg'}>
       <ReactPlayer
@@ -10,6 +10,8 @@ const VideoPlayer = ({ url }) => {
         controls={true}
         playing={false} // Set to true to auto-play
         width="100%"
+        onPlay={handlePlay}
+        onPause={handlePause}
       />
     </Box>
   );
